0bdba40392c7c4e8c26e8dd27b7a662e517709ef
Sam_Yang <sam.yang@smms.com.tw>
2024-08-19 09:58:52 +0800

    add real env

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { unstable_noStore as noStore } from "next/cache";
 
 export default function Home() {
   noStore();
-  if (process.env.SOMEENV) {
-    return <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-green-200">{process.env.SOMEENV}</main>;
+  if (process.env.NEXT_PUBLIC_SOMEENV) {
+    return <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-green-200">{process.env.NEXT_PUBLIC_SOMEENV}</main>;
   } else {
     return <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-300">NOTTHINGGGGGGGGGG</main>;
   }
